386f308880350a226eb1f3608ce79b65f6d783c9
Leo <leoaguilarsanchez97@gmail.com>
2020-11-17 11:03:06 -0600

    Agregada ruta para crear usuario

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
+
+connectDB();
 
 app.get('/', (req, res) => { res.send(`API escuchando`) });
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server iniciado en el puerto ${PORT}`));
