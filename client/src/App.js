1822b1114ae1fd421c22ac85c1ac13cb7b2f3fe6
Leo <leoaguilarsanchez97@gmail.com>
2020-11-23 15:18:49 -0600

    Agregado Login, Logout, Register completo.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,48 @@
 
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Alert from './components/layout/alert';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import setAuthToken from './utils/setAuthToken';
+import { loadUser } from './actions/auth';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 import './App.css';
 
-const App = () =>
-  <Fragment>
-    <Navbar></Navbar>
-    <Landing></Landing>
-  </Fragment>
-  ;
 
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (<Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar></Navbar>
+        <Route exact path='/' component={Landing}></Route>
+        <section className="container">
+          <Alert></Alert>
+          <Switch>
+            <Route exact path="/register" component={Register}></Route>
+            <Route exact path="/login" component={Login}></Route>
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
+  );
+}
 export default App;
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,85 @@
+import React, { Fragment, useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert';
+import { register, loadUser } from '../../actions/auth';
+import PropTypes from 'prop-types';
+
+const Register = ({ setAlert, register, isAuthenticated }) => {
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: ''
+    });
+
+    const { name, email, password, password2 } = formData;
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = async e => {
+        e.preventDefault();
+
+        if (password !== password2) {
+            setAlert('Passwords do not match', 'danger');
+        } else {
+            register({ name, email, password });
+        }
+    }
+
+    if (isAuthenticated) {
+        return <Redirect to="/dashboard"></Redirect>;
+    }
+
+    return (
+        <Fragment>
+            <h1 class="large text-primary">Sign Up</h1>
+            <p class="lead"><i class="fas fa-user"></i> Create Your Account</p>
+            <form class="form" action="create-profile.html" onSubmit={e => onSubmit(e)}>
+                <div class="form-group">
+                    <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} />
+                </div>
+                <div class="form-group">
+                    <input type="email" placeholder="Email Address" value={email} onChange={e => onChange(e)} name="email" />
+                    <small class="form-text"
+                    >This site uses Gravatar so if you want a profile image, use a
+              Gravatar email</small
+                    >
+                </div>
+                <div class="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={password} onChange={e => onChange(e)}
+                    />
+                </div>
+                <div class="form-group">
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
+                        name="password2"
+                        value={password2} onChange={e => onChange(e)}
+                    />
+                </div>
+                <input type="submit" class="btn btn-primary" value="Register" />
+            </form>
+            <p class="my-1">
+                Already have an account? <Link to="/login">Sign In</Link>
+            </p>
+        </Fragment>
+    )
+};
+
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
+};
+
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
